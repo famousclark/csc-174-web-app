ba30976df02482eed4355754de76d10fd16bc519
famousclark <clark.famous@gmail.com>
2018-09-06 17:01:56 -0400

    #commit 1 - first basic push

---
+++ inc/app-nav.php
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li>
+            <a href='index.php'>Home</a>
+        </li>
+
+        <li>
+            <a href='about.php'>About</a>
+        </li>
+        <li
+            ><a href='projects.php'>Projects</a>
+        </li>
+        <li>
+            <a href='research.php'>Research</a>
+        </li>
+    </ul>
+</nav>
---
+++ inc/app.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Famous - Index </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/main.css" />
+
+    <link rel="stylesheet" type="text/css" href ="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css"/>
+
+    <script src="scripts/main.js"></script>
+ </head>   
