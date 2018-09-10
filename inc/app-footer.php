3ef19101b6d16a939b179a819668c82a2eefe248
famousclark <clark.famous@gmail.com>
2018-09-10 16:36:28 -0400

    #commit - styling complete, add content

---
+++ inc/app-footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <div class="footer center align container">
+        <div>I know, Im awesome</div>
+        <div class="split"> </div>
+        <div class="footer bottom"></div>
+    </div>
+</div>
---
+++ inc/app-meta.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Famous - Index </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/app-body.css?<?php echo time();?>"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/app-basics.css?<?php echo time();?>"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/app-header.css?<?php echo time();?>"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/app-footer.css?<?php echo time();?>"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/app-container.css?<?php echo time();?>"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/app-nav.css?<?php echo time();?>"/>
+    
+ </head>   
